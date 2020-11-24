662c61f105dfad2faf015856db44ac07fb7d2a11
liuching <benjack30518@gmail.com>
2020-11-24 08:01:32 +0800

    1124 algorithm, LDLab5

--- NTHU_OJ/algorithm.cpp
+++ NTHU_OJ/algorithm.cpp
@@ -1,67 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int idx;
-int mmap[1500][1500];
+template<typename T> using vec = vector<T>;
 
-void puzzle(int power, int row, int column, int r, int c)
-{
-    if (power == -1)
-        return;
-    
-    int re = row + (1<<power);
-    int ce = column + (1<<power);
-    int sr[4] = {re-1, re-1, re, re};
-    int sc[4] = {ce-1, ce, ce-1, ce};
-    
-    int which = 0;
-    if (r >= re) which += 2;
-    if (c >= ce) which += 1;
-    
-    sr[which] = r;
-    sc[which] = c;
-    
-    for (int i = 0; i < 4; i++)
-    {
-        if (i == which)
-            continue;
-        
-        int pr = (i >= 2 ? row + (1<<power) : row);
-        int pc = (i % 2 ? column + (1<<power) : column);
-        
-        puzzle(power-1, pr, pc, sr[i], sc[i]);
-    }
-    
-    for (int i = re-1; i < re+1; i++)
-        for (int k = ce-1; k < ce+1; k++)
-            mmap[i][k] = idx;
-    idx++;
-    
-    int pr = (which >= 2 ? row + (1<<power) : row);
-    int pc = (which % 2 ? column + (1<<power) : column);
-    puzzle(power-1, pr, pc, sr[which], sc[which]);
-}
+int dp[1010][100010];
 
 int main()
 {
-    int n;
-    cin >> n;
+    int c;
+    cin >> c;
     
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 1010; i++)
+        dp[i][0] = 0;
+    for (int i = 0; i < 100010; i++)
+        dp[0][i] = 0;
+    
+    while (c--)
     {
-        if (i)
-            cout << '\n';
+        int n, sum = 0;
+        cin >> n;
         
-        int w, r, c;
-        cin >> w >> c >> r;
-        idx = 1;
+        vec<int> _list(n);
+        for (auto &i : _list)
+        {
+            cin >> i;
+            sum += i;
+        }
         
-        puzzle(w-1, 0, 0, r, c);
+        if (sum % 2)
+        {
+            cout << "QAQ\n";
+            continue;
+        }
         
-        mmap[r][c] = -1;
-        
-        for (int i = 0; i < (1<<w); i++)
-            for (int k = 0; k < (1<<w); k++)
-                cout << mmap[i][k] << " \n"[k == (1<<w)-1];
+        for (int i = 1; i <= n; i++)
+            for (int k = 1; k <= sum/2; k++)
+            {
+                if (_list[i-1] > k)
+                    dp[i][k] = dp[i-1][k];
+                else
+                    dp[i][k] = max(dp[i-1][k - _list[i-1]] + _list[i-1], dp[i-1][k]);
+            }
+            
+        cout << (dp[n][sum/2] == sum/2 ? "Yes\n" : "QAQ\n");
     }
 }
--- NTHU_OJ/template2.cpp
+++ NTHU_OJ/template2.cpp
@@ -1,10 +1,56 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <cstring>
+using namespace std;
+int N, T;
+int seq[1005];
+bool DP[1005][100005];
+// bool able[100005];
 int main()
 {
-	int i;
-	std::cin >> i;
-	const int j = i;
-
-	std::cout << i << ' ' << j << '\n';
+    cin >> T;
+    while(T--){
+        memset(seq, 0, sizeof(seq));
+        memset(DP, 0, sizeof(DP));
+        cin >> N;
+        int Maxnum = 0;
+        int totalSum = 0;
+        for (int i = 1; i <= N; i++){
+            cin >> seq[i];
+            Maxnum = max(Maxnum, seq[i]);
+            totalSum += seq[i];
+        }
+        if((totalSum % 2 == 1 )|| Maxnum * 2 > totalSum){
+            cout << "No\n";
+            continue;
+        }else if(Maxnum * 2 == totalSum){
+            cout << "Yes\n";
+            continue;
+        }
+        // memset(able, 0, sizeof(able));
+        // able[0] = true;
+        DP[0][0] = true;
+        for (int i = 1; i <= N; i++){
+            cout << "Now doing " << seq[i] << endl;
+            DP[i][seq[i]] = true;
+            
+            for (int j = 1; j <= totalSum; j++){
+                if(j - seq[i] >= 0 && i > 1){
+                    DP[i][j] = DP[i][j] || DP[i - 1][j - seq[i]];
+                }
+                if(i > 1){
+                    DP[i][j] = DP[i][j] || DP[i-1][j];
+                }
+            }
+        }
+        // for (int i = 1; i <= N; i++){
+        //     for (int j = 1; j <= totalSum; j++)
+        //     {
+        //         cout << DP[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << "\nAns\n";
+        cout <<( DP[N][totalSum / 2] == 1 ? "Yes\n" : "No\n");
+    }
+        return 0;
 }
